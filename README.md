5c9427c89bb12c352f7589000a0ad5c0a5b8102d
Supriya090 <khadkasupriya76@gmail.com>
2021-12-02 22:53:09 +0545

    Entrance fee function added and tested

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 1. Users can enter lottery with ETH based on USD fee
 2. An admin decides when the lottery will end
 3. The lottery will select a random user
+
+## How do we want to test this?
+
+1. `mainnet-fork`
+2. `development` with mocks
+3. `testnet`
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,13 +2,17 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
-contract Lottery{
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
+contract Lottery{
+ 
     address payable[] public players;
     uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
 
-    constructor(){
+    constructor(address _priceFeedAddress){
         usdEntryFee = 50*(10**18);
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
 
     }
 
@@ -18,8 +22,15 @@ contract Lottery{
     }
 
     function getEntranceFee() public view returns (uint256){
-
+        (,int price,,,) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10;    //18 decimals
+        // $50, $2,000 / ETH
+        // 50/2,000
+        // 50 * 100000 / 2000
+        uint256 costToEnter = (usdEntryFee * 10 ** 18) / adjustedPrice;
+        return costToEnter;
     }
+
     function startLottery() public{}
     function endLottery() public{}
 
