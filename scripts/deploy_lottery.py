374881f3d3dac42209368e883a8163b1be619c24
Supriya090 <khadkasupriya76@gmail.com>
2021-12-23 16:52:56 +0545

    Lottery started, entered and ended for development environment

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,5 +1,6 @@
-from scripts.helpful_scripts import get_account, get_contract
+from scripts.helpful_scripts import get_account, get_contract, fund_with_link
 from brownie import Lottery, config, network
+import time
 
 
 def deploy_lottery():
@@ -17,5 +18,38 @@ def deploy_lottery():
     print("Deployed Lottery!")
 
 
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    starting_txn = lottery.startLottery({"from": account})
+    starting_txn.wait(1)
+    print("The lottery begins!")
+
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery!")
+
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    # first fund the function
+    # then end the lottery
+    tx = fund_with_link(lottery.address)
+    tx.wait(1)
+    ending_transaction = lottery.endLottery({"from": account})
+    ending_transaction.wait(1)
+    time.sleep(60)
+    print(f"{lottery.recentWinner()} is the new winner!")
+
+
 def main():
     deploy_lottery()
+    start_lottery()
+    enter_lottery()
+    end_lottery()
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -5,7 +5,8 @@ from brownie import (
     Contract,
     MockV3Aggregator,
     VRFCoordinatorMock,
-    LinkToken
+    LinkToken,
+    interface
 )
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
@@ -72,3 +73,18 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
     link_token = LinkToken.deploy({"from": account})
     VRFCoordinatorMock.deploy(link_token.address, {"from": account})
     print("Deploy")
+
+
+def fund_with_link(contract_address, account=None, link_token=None, amount=100000000000000000):  # 0.1 LINK
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    tx = link_token.transfer(contract_address, amount, {"from": account})
+
+    # Contacting with interface
+
+    # link_token_contract = interface.LinkTokenInterface(link_token.address)
+    # tx = link_token_contract.transfer(
+    #     contract_address, amount, {"from": account})
+    tx.wait(1)
+    print("Fund contract!")
+    return tx
