66e82430b031874a821a90896f29adea838e39b0
Supriya090 <khadkasupriya76@gmail.com>
2021-12-24 16:37:28 +0545

    Unit testing completed

--- tests/test_lottery_unit.py
+++ tests/test_lottery_unit.py
@@ -3,7 +3,7 @@ from brownie import Lottery, accounts, config, network, exceptions
 from scripts.deploy_lottery import deploy_lottery
 from web3 import Web3
 import pytest
-from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, fund_with_link, get_account
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, fund_with_link, get_account, get_contract
 
 
 def test_get_entrance_fee():
@@ -67,3 +67,33 @@ def test_can_end_lottery():
 
     # Assert
     assert lottery.lottery_state() == 2
+
+
+def test_can_pick_winner_correctly():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=1),
+                  "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=2),
+                  "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+
+    starting_balance_of_account = account.balance()
+    balance_of_lottery = lottery.balance()
+
+    transaction = lottery.endLottery({"from": account})
+    request_id = transaction.events["RequestedRandomness"]["requestId"]
+    STATIC_RNG = 777
+    get_contract("vrf_coordinator").callBackWithRandomness(
+        request_id, STATIC_RNG, lottery.address, {"from": account})
+
+    # Account 0 will be the winner as 777 % 3 = 0
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
+    assert account.balance() == starting_balance_of_account + balance_of_lottery
